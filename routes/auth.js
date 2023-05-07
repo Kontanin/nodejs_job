e5b7e9c1d04e6cd9359cde44bb848b704c3870ae
Kontanin <boss12853@gmail.com>
2023-05-07 16:37:48 +0700

    try

--- routes/auth.js
+++ routes/auth.js
@@ -7,7 +7,7 @@ const { register, login, updateUser } = require('../controllers/auth');
 const rateLimiter = require('express-rate-limit');
 const apiLimiter = rateLimiter({
   windowMs: 15 * 60 * 1000, // 15 minutes
-  max: 10,
+  max: 1,
   message: {
     msg: 'Too many requests from this IP, please try again after 15 minutes',
   },
